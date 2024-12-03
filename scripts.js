fb9c9571e61e10d1729eaa0075d800f7687d6ffd
EpicEfeathers <76843932+EpicEfeathers@users.noreply.github.com>
2024-12-03 17:30:26 -0500

    Light/Dark mode toggle

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
+// Footer Year
 document.getElementById("footer").textContent = new Date().getFullYear()
+
+// Light/Dark mode toggle
+const toggleButton = document.getElementById("toggle");
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+})
+
+//body.classList.toggle('dark-mode');
