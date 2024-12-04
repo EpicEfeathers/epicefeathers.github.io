7b22ff8edf125a3449518c5ffc49bbc4e467b57f
EpicEfeathers <76843932+EpicEfeathers@users.noreply.github.com>
2024-12-03 22:39:14 -0500

    Formatted icon

--- scripts.js
+++ scripts.js
@@ -3,10 +3,18 @@ document.getElementById("footer").textContent = new Date().getFullYear()
 
 // Light/Dark mode toggle
 const toggleButton = document.getElementById("toggle");
-const body = document.body;
+const toggleIcon = document.getElementById("toggle-icon");
 
 toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
+    document.body.classList.toggle('dark-mode');
+
+    setTimeout(() => {
+        if (toggleIcon.classList.contains('bi-moon-stars-fill')) {
+            toggleIcon.classList.replace('bi-moon-stars-fill', 'bi-sun');
+        } else {
+            toggleIcon.classList.replace('bi-sun', 'bi-moon-stars-fill');
+        }
+    }, 50);
 })
 
 //body.classList.toggle('dark-mode');
