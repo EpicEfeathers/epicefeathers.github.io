0a87c55bd659f36593bfb3a8d67b1e60016cf4d0
EpicEfeathers <76843932+EpicEfeathers@users.noreply.github.com>
2025-03-27 09:55:52 -0400

    Fix overscroll bug

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ const toggleButton = document.getElementById("toggle");
 const toggleIcon = document.getElementById("toggle-icon");
 
 toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
+    document.documentElement.classList.toggle('dark-mode');
 
     setTimeout(() => {
         if (toggleIcon.classList.contains('bi-moon-stars-fill')) {
@@ -16,5 +16,3 @@ toggleButton.addEventListener('click', () => {
         }
     }, 50);
 })
-
-//body.classList.toggle('dark-mode');
